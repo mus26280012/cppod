cfd38260c71d351aa11b48a8e2fde2c5ef3cdcb9
周杰 <454071867@qq.com>
2018-08-25 11:50:30 +0800

    no message

---
+++ cppod/cppod/UserHelpView/Model/ZJTGetModel.m
@@ -0,0 +1,17 @@
+//
+//  ZJTGetModel.m
+//  HappyBJL
+//
+//  Created by chen minyue on 2018/6/17.
+//  Copyright © 2018年 B. All rights reserved.
+//
+
+#import "ZJTGetModel.h"
+
+@implementation ZJTGetModel
+
+@end
+
+@implementation ZJTResult
+
+@end
---
+++ cppod/cppod/UserHelpView/ZJTUserHelpView.m
@@ -0,0 +1,29 @@
+//
+//  PShowView.m
+//  ShangKOrganization
+//
+//  Created by chen minyue on 2018/4/19.
+//  Copyright © 2018年 Fbw. All rights reserved.
+//
+
+#import "ZJTUserHelpView.h"
+
+@implementation ZJTUserHelpView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+-(void)layoutSubviews {
+    self.button1.tag=1;
+    self.button2.tag=2;
+    self.button3.tag=3;
+    self.button4.tag=4;
+    self.button5.tag=5;
+}
+
+@end
